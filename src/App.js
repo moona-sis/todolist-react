6c13984ef3bde2f6c3fc2f368c1374fccdd81e9b
moona-sis <mouna.sismail15@gmail.com>
2020-10-06 13:29:09 +0100

    todolist-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Navbar from './componenents/Navbar'
+
 
 function App() {
+const [input, setInput] = useState('')
+const [todo, setTodo] = useState ([])
+console.log(todo)
+const handleAddTodo = ()=> {
+  if (input === ''){
+    alert ('please insert a todo')
+  }else{
+    const todolist =[...todo, input]
+    setTodo (todolist)
+  }
+}
+console.log(input)
+  
+  const removeItem=(index)=>{
+    console.log(index)
+    const removeItem = todo.filter(td => todo.indexOf(td) !== index)
+    setTodo(removeItem)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar title = 'Todo app'/>
+      <div className='container'>
+        <div>
+      
+      <input onChange={(e)=> setInput(e.target.value)} type ='text' placeholder='add todo here'/>
+      <button onClick={handleAddTodo} type='submite'>Add</button>
+      </div>
+      <div className='content'>
+  {todo.map((td,index) =>(<div className='list' key={index}>
+   <div className='todo' >{td} </div>
+    <div onClick={()=>removeItem (todo.indexOf(td))} className='close'>X</div>
+    
+  </div>
+  
+  
+  )) }
+  </div>
+  
+ 
+  </div>
+     
     </div>
   );
 }
---
+++ src/componenents/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = (props) => {
+    return (
+        <div className='navbar'>{props.title}</div>
+    )
+}
+
+export default Navbar
